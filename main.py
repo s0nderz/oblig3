94b171f61e94b49c2b5d2417a9655fa3304b0783
s0nderz <only94-@hotmail.com>
2023-10-25 12:56:19 +0200

    nvm

--- main.py
+++ main.py
@@ -1,28 +1,15 @@
-import random
-
-year_list = []
-i = 0
-while i < 100:
-    year = random.randrange(0, 10000)
-    year_list.append(year)
-    i += 1
-
-def random_year():
-    for i in year_list:
-        return i
-
-def isLeapYear():
-    if random_year() % 4000 == 0:
-        print(str(random_year()) + " is not a leap year")
+def isLeapYear(year):
+    if year % 4000 == 0:
+        print(str(year) + " is not a leap year")
         return False
-    elif random_year() % 4 == 0 and random_year() % 100 != 0:
-        print(str(random_year()) + " is a leap year")
+    elif year % 4 == 0 and year % 100 != 0:
+        print(str(year) + " is a leap year")
         return True
-    elif random_year() % 400 == 0:
-        print(str(random_year()) + " is a leap year")
+    elif year % 400 == 0:
+        print(str(year) + " is a leap year")
         return True
     else:
-        print(str(random_year()) + " is not a leap year")
+        print(str(year) + " is not a leap year")
         return False
 
 
